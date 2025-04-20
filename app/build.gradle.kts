163f33ce21b34f3f3b9a92a61fe8a78115146954
6041579+Chrispassold <6041579+Chrispassold@users.noreply.github.com>
2025-04-19 21:56:36 -0300

    feat: project initial setup
    - Removes `MainActivity.kt`.
    - Updates `settings.gradle.kts` with project name and build file.
    - Updates `app/build.gradle.kts` to configure debug/release, changes java/kotlin version to 17.
    - Updates `gradle/libs.versions.toml`.
    - Updates `AndroidManifest.xml` and set the entrypoint to `LoginActivity`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,14 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = false
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
+        }
         release {
             isMinifyEnabled = false
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -29,11 +35,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
+rootProject.name = "AskBuddy"
+rootProject.buildFileName = "build.gradle.kts"
+
 pluginManagement {
     repositories {
         google {
@@ -11,13 +14,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
+    @Suppress("UnstableApiUsage") repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage") repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "AskBuddy"
 include(":app")
