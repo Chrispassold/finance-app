1bb2e061526e85eb8a07b77c1536d7e2fced5f60
6041579+Chrispassold <6041579+Chrispassold@users.noreply.github.com>
2025-05-29 14:38:33 -0300

    feat: add `resultWithContext` extension function
    This commit introduces a new suspend extension function `resultWithContext` in the `core` module.
    
    This function simplifies executing a suspend block within a specified `CoroutineContext` and wrapping the result (or any thrown exception) in a `Result` type.
    
    It leverages `runCatching` to handle potential exceptions during the execution of the block with `withContext`.

---
+++ core/src/main/java/com/chrispassold/core/Extensions.kt
@@ -0,0 +1,14 @@
+package com.chrispassold.core
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+
+suspend fun <T> resultWithContext(
+    context: CoroutineContext,
+    block: suspend CoroutineScope.() -> T,
+): Result<T> {
+    return runCatching {
+        withContext(context, block)
+    }
+}
